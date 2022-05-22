76e6436386b95e41cc78807bc73b76dfe0c9765c
sakamo09 <teamilkyui326@gmail.com>
2022-05-23 00:44:50 +0900

    テストコードを記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name {""}
+    first_name {""}
+    last_name_kana {""}
+    first_name_kana {""}
+    birthday {""}
+    email{""}
+   
+
+  end
+end
