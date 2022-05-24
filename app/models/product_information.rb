12749f1ea78aa0d15213a550a32ac33721ffad77
sakamo09 <teamilkyui326@gmail.com>
2022-05-24 15:03:06 +0900

    ActiveHashの設定

--- app/models/product_information.rb
+++ app/models/product_information.rb
@@ -1,5 +1,12 @@
 class ProductInformation < ApplicationRecord
   has_one_attached :image
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
   # belongs_to :user 
   # has_one :purchase_record
 
