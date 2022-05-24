235d6045590554cddcd2df1fbcf284460d4630cf
sakamo09 <teamilkyui326@gmail.com>
2022-05-24 10:52:28 +0900

    アクティブストレージの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+
+
+
+
+
+
+
+  private
+
+  def product_informations_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/product_information.rb
+++ app/models/product_information.rb
@@ -1,2 +1,6 @@
 class ProductInformation < ApplicationRecord
+  has_one_attached :image
+  # belongs_to :user 
+  # has_one :purchase_record
+
 end
