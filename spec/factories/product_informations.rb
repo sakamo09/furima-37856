9a44646083745385f15b09f1cc24726237febd43
sakamo09 <teamilkyui326@gmail.com>
2022-05-25 18:55:55 +0900

    単体テストコードの設定

--- spec/factories/product_informations.rb
+++ spec/factories/product_informations.rb
@@ -1,14 +1,22 @@
 FactoryBot.define do
   factory :product_information do
-    image {}
+    association :user
     name {'花'}
     description {'手作りです'}
-    category_id {'レディース'}
-    situation_id {'新品'}
-    load_id {'送料込み(出品者負担)'}
-    area_id {'東京都'}
-    period_id {'2~3日で発送'}
-    price {'1000'}
+    category_id {2}
+    situation_id {2}
+    load_id {2}
+    area_id {2}
+    period_id {2}
+    price {1000}
+  
+    
+
+
+
+    after(:build) do |product_information|
+      product_information.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
     
     
   end
--- spec/models/product_information_spec.rb
+++ spec/models/product_information_spec.rb
@@ -1,5 +1,87 @@
 require 'rails_helper'
 
 RSpec.describe ProductInformation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @product_information= FactoryBot.build(:product_information)
+  end
+
+  describe '商品出品機能' do
+    
+    context '商品出品できるとき' do
+      it ' image,name,description,category_id,situation_id,load_id,area_id,period_id,priceが全て入力されているとき' do
+        expect(@product_information).to be_valid
+      end
+    end
+
+    context '新規登録できないとき' do
+      
+     it 'image,が空では登録できない' do
+      @product_information.image = nil
+      @product_information.valid?
+       expect(@product_information.errors.full_messages).to include("Image can't be blank")
+      end
+
+     it 'name,が空では登録できない' do
+      @product_information.name = ''
+      @product_information.valid?
+       expect(@product_information.errors.full_messages).to include("Name can't be blank")
+      end
+
+     it 'description,が空では登録できない' do
+      @product_information.description = ''
+      @product_information.valid?
+       expect(@product_information.errors.full_messages).to include("Description can't be blank")
+      end
+
+     it 'category_id,が空では登録できない' do
+      @product_information.category_id = ''
+      @product_information.valid?
+       expect(@product_information.errors.full_messages).to include("Category can't be blank")
+      end
+
+     it 'situation_id,が空では登録できない' do
+      @product_information.situation_id = ''
+      @product_information.valid?
+       expect(@product_information.errors.full_messages).to include("Situation can't be blank")
+      end
+
+     it 'load_id,が空では登録できない' do
+      @product_information.load_id = ''
+      @product_information.valid?
+       expect(@product_information.errors.full_messages).to include("Load can't be blank")
+      end
+
+     it 'area_id,が空では登録できない' do
+      @product_information.area_id = ''
+      @product_information.valid?
+       expect(@product_information.errors.full_messages).to include("Area can't be blank")
+      end
+   
+     it 'period_id,が空では登録できない' do
+      @product_information.period_id = ''
+      @product_information.valid?
+       expect(@product_information.errors.full_messages).to include("Period can't be blank")
+      end
+     
+      it 'price,が空では登録できない' do
+        @product_information.price = ''
+        @product_information.valid?
+         expect(@product_information.errors.full_messages).to include("Price can't be blank")
+        end
+     
+      it 'priceが¥300~¥9,999,999でないと登録できない' do
+          @product_information.price = '10'
+          @product_information.valid?
+           expect(@product_information.errors.full_messages).to include("Price is invalid")
+          end
+     
+       it 'priceが半角数値でないと登録できない' do
+          @product_information.image = nil
+          @product_information.valid?
+           expect(@product_information.errors.full_messages).to include("Image can't be blank")
+       end
+   
+
+   end
+ end
 end
