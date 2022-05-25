03ccdbf918f1d190f00c911e8b0ca177fd197466
sakamo09 <teamilkyui326@gmail.com>
2022-05-25 16:13:04 +0900

    Factory Bot編集

--- spec/factories/product_informations.rb
+++ spec/factories/product_informations.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :product_information do
+    image {}
+    name {'花'}
+    description {'手作りです'}
+    category_id {'レディース'}
+    situation_id {'新品'}
+    load_id {'送料込み(出品者負担)'}
+    area_id {'東京都'}
+    period_id {'2~3日で発送'}
+    price {'1000'}
+    
     
   end
 end
