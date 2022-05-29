2afcc15e88e7ae0114d7c3ebc51c65ed02b2fd10
sakamo09 <teamilkyui326@gmail.com>
2022-05-29 16:51:22 +0900

    スペルミスなおし

--- app/controllers/product_informations_controller.rb
+++ app/controllers/product_informations_controller.rb
@@ -1,6 +1,6 @@
 class ProductInformationsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show,]
-  before_action :set_tweet, only: [:show, :edit, :update, :destory]
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
 
   
