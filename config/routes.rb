2cfc104d3982ec7276fa612151d58c5639288078
sakamo09 <teamilkyui326@gmail.com>
2022-05-30 19:57:15 +0900

    ルーテイングネスト設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "product_informations#index"
   resources :product_informations 
+
+  resources :product_informations do
+    resources :purchase_records
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
