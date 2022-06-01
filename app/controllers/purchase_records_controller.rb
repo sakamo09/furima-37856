2dc1b3525ae9127c30f7759135363db0e9d412f3
sakamo09 <teamilkyui326@gmail.com>
2022-06-01 09:11:36 +0900

    'pry-rails導入

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -6,5 +6,16 @@ class PurchaseRecordsController < ApplicationController
   end
 
 
+  def create
+    # PurchaseRecord.create(purchase_record_params)
+  end
+
+
+
+  # private
+  # def purchase_record_params
+  #   params.require(:purchase_record).permit(:postal_code, :area_id, :municipalities, :address, :building_name, :telephone_number)
+  # end
+
 
 end
