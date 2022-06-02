444c175905d2275df7ed67e1d95b8f8998aa96d6
sakamo09 <teamilkyui326@gmail.com>
2022-06-02 19:47:11 +0900

    売れていない商品にはsoidが出ないようにした

--- app/controllers/product_informations_controller.rb
+++ app/controllers/product_informations_controller.rb
@@ -1,6 +1,6 @@
 class ProductInformationsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show,]
-  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy] 
   before_action :contributor_confirmation, only: [:edit, :destroy]
  
 
